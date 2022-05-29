d89ef51ee4850f23cc6977651ecef5bbf4cfb33c
Wang <tw1320@ic.ac.uk>
2022-05-29 13:10:53 +0100

    minor changes

--- braids/braid.py
+++ braids/braid.py
@@ -1,11 +1,14 @@
 '''Constructs braid group objects using word'''
 
+import numpy as np
+import sympy as sp
+
 class braid(object):
 
     def __init__(self, word):
         """
         Constructs braid via word.
-        - word: a list of integers e.g. [1,-2,3]=sigma_1 sigma_2^{-1} sigma_3
+        - word: a np array of integers e.g. [1,-2,3]=sigma_1 sigma_2^{-1} sigma_3
         """
         self.word = word
     
@@ -18,10 +21,34 @@ class braid(object):
 
     def maingen(self):
         """Returns the main generator. (min index)"""
-        pass
+        a = self.word[0]
+        for i in range(self.word):
+            m = self.word[i]
+            if abs(m)<a and -1*m in self.word:
+                a = m
+        return a
+
+    def mainhandle(self):
+        """Returns main handle of the word."""
+        j = self.maingen()
+        
+
     def isreducible(self):
-        """"""
-        pass
+        """Checks if word is permitted."""
+        flag = True
+        handle = self.handle()
+        j = handle[0]
+        if j+1 in set(handle[1:-1]) and -1*j-1 in set(handle[1:-1]):
+            flag = False
+        return flag
+        
+    def handle(self):
+        """Returns the outermost handle in a word."""
+
+
+
+
+    
 
         
 
--- braids/dehornoy.py
+++ braids/dehornoy.py
@@ -1,10 +1,18 @@
 """implements Dehornoy's algortihm"""
 
-def handle():
-    """Returns the outermost handle in a word."""
-    pass
-
-
 def dehornoy():
     """Returns a reduced word with outermost handle removed."""
     pass
+
+
+# pseudo-code
+
+# for handles in word: (of the form sigma_j^-1 v sigma_j^1)
+#   handle[-1]. handl[0] = 0
+#   
+#
+#
+#
+#
+#
+#
