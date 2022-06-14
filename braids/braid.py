94078af4eb34e5e58ff68e2c072fef72e7807e5d
isomorphicdude <iamthesenate@qq.com>
2022-06-15 00:18:00 +0100

    complete most parts

--- braids/braid.py
+++ braids/braid.py
@@ -18,15 +18,12 @@ class braid(object):
     def __repr__(self):
         return type(self).__name__ + "("+repr(self.word)+")"      
 
-    def isreduced(self):
-        """Checks if word is reduced."""
-        
 
     def maingen(self):
-        """Returns the main generator. (min index)"""
+        """Returns the main generator as integer."""
         w = np.array(self.word)
         m = abs(w)
-        return np.argmin(m)
+        return min(m)
 
     def mainhandle(self):
         """Returns main handle of the word.(if there are multiple, return leftmost one)"""
@@ -36,33 +33,56 @@ class braid(object):
             pos = self.word.index(j)
             neg = self.word.index(-1*j)
             if pos > neg:
-                out = self.word[neg:pos+1]
+                out = braid(self.word[neg:pos+1])
             else:
-                out = self.word[pos:neg+1]
-            if not self.isreducible(out):
-                print("Not permitted")
+                out = braid(self.word[pos:neg+1])
+            if not self.ispermitted(out):
+                out = "Handle found is not permitted"
             return out
         else:
             print("No main handle found, fully reduced")
 
-    def isreducible(self, handle):
+    def ispermitted(self, handle):
         """Checks if a handle is permitted."""
         flag = True
-        j = abs(handle[0])
-        if j+1 in set(handle[1:-1]) and -1*j-1 in set(handle[1:-1]):
+        w = handle.word
+        j = abs(w[0])
+        if j+1 in set(w[1:-1]) and -1*j-1 in set(w[1:-1]):
             flag = False
-            print("Not permitted")
+            # print("Not permitted")
         return flag
-        
+
+    def isreduced(self):
+        """Checks if word is reduced."""
+        if not self.mainhandle():
+            return True
+        else:
+            return False
+
     def left_handle(self):
         """Returns the leftmost handle in a word."""
-        n = len(self.word)
-        j = None
-        for x in self.word:
-            if -1*x in self.word:
-                j = self.word.index(-1*x)
+        w = self.word
+        n = len(w)
+        j = n
+        k = 0
+        assert not self.isreduced()
 
-         
+        for i in range(n):
+            x = w[i]
+            if -1*x in w[i:j]:
+                if self.word.index(-1*x)<j:
+                    j = self.word.index(-1*x)+1
+                    k = i
+                    print(f"k is {i}")
+                    print(f"j is {j}")
+        if (j+1)<=n-1:
+            out = w[k:j]
+            print(k)
+        else:
+            out = w[k:-1]
+            print(k)
+        return braid(out)
+
 
     def permutation(self):
         """The permutation to which a braid in [0,1] corresponds."""
