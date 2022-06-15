d0cdb0a21c87e8ad5f669c57a3f6667292bdea6f
isomorphicdude <iamthesenate@qq.com>
2022-06-15 12:36:09 +0100

    add reduction_tools

---
+++ braids/reduction_tools.py
@@ -0,0 +1,12 @@
+"""Tools for reducing the braid."""
+
+def free_reduce(handle):
+    """Freely reduces a handle."""
+    pass
+
+def zero_reduce(beta):
+    """Removes all empty letters in braid."""
+    pass
+
+def double(beta):
+    pass
